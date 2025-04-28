070e2fbdfdbae5e759bfbd9e2acbc11c5e148f2d
Jefferson Alaiza <jeffersonaalaiza@iskolarngbayan.pup.edu.ph>
2025-04-28 16:37:45 +0800

    banana

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def read_root():
 def read_items(skip: int = 0, limit: int = 10):
     return list(items.values())[skip: skip + limit]
 
+@app.get('/banana/')
+def read_banana():
+    return {"banana": "banana"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     if item_id not in items:
